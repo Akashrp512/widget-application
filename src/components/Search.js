a2bd6e945b71f5de8a1ca708f48dadb69bd43386
Akash RP <akashrp512@gmail.com>
2022-11-12 13:18:42 +0530

    commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,50 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm]=useState('');
-console.log('i run with every render'); 
-    useEffect(()=>{
-         console.log('i run after every render')
-    }, [term]);
+    const [term, setTerm]=useState('React');
+    const [results, setResults] =useState([]);
+   
 
+    useEffect(()=>{
+        const search = async () =>{
+          const {data} =  await axios.get('https://en.wikipedia.org/w/api.php', {
+             params: {
+                action: 'query',
+                list: 'search',
+                origin: '*',
+                format: 'json',
+                srsearch: term,
+            },
+            });
+
+            setResults(data.query.search);
+
+        };
+
+        setTimeout(() => {
+            if (term) {
+                search();
+            }
+        }, 500);
+    }, [term]); 
+
+
+   const renderedResults =results.map((result)=>{
+        return (
+        <div className='item' key={result.pageid}>
+        <div className='right floated content'>
+            <a className='ui button' href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+        </div>
+        <div className='content'>
+            <div className='header'>
+                {result.title}
+            </div>
+            <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
+        </div>
+           </div>
+        );
+   }) 
   return (
     <div>
       <div className='ui form' >
@@ -17,6 +55,9 @@ console.log('i run with every render');
             value={term} 
             onChange={(e )=> setTerm(e.target.value)}
              />
+        </div>
+        <div className='ui celled list'>
+            {renderedResults}
         </div>
       </div>
     </div>
