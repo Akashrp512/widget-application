ce8abe7c3bc6487222e9e208fa497c866d4bf972
Akash RP <akashrp512@gmail.com>
2022-11-16 22:14:14 +0530

    commit

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,7 @@
+
 import React, { useState} from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 
 
@@ -20,8 +22,15 @@ export const options= [
 
 const Translate = () => {
     const [language, setLanguage] = useState(options[0]);
+    const [text, setText] =useState('');
   return (
     <div>
+    <div className='ui form'>
+    <div className='field'>
+    <label>Enter Text</label>
+    <input value={text} onChange= {(e)=> setText(e.target.value)} />
+    </div>
+    </div>
       <Dropdown
       label='Select a Language'  
       options={options}
@@ -29,6 +38,9 @@ const Translate = () => {
       onSelectedChange={setLanguage}
 
       />
+      <hr />
+      <h3 className='ui header'>Output</h3>
+      <Convert text={text} language={language} />
     </div>
   );
 };
