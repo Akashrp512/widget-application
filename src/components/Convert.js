2078b7e68e8e2792cdc0c8d9684c0350f8da2d42
Akash RP <akashrp512@gmail.com>
2022-11-16 22:14:18 +0530

    commit

---
+++ src/components/Convert.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
+
+const Convert = ({language, text}) => {
+    useEffect(()=>{
+   axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+    params: {
+        q: text,
+        target: language.value,
+        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+    }
+   })
+    }, [language, text])
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Convert
