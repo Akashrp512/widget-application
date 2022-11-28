1ea227cd5c729529dee83aea426931505dbfd9e5
Akash RP <akashrp512@gmail.com>
2022-11-28 10:18:08 +0530

    Added Standard approach of Routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Search from './components/Search';
 import { items } from './constants';
 import { options } from './constants';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 const showAccordion =() =>{
   if(window.location.pathname === '/'){
@@ -29,13 +30,22 @@ const showTranslate =() =>{
 
 const App = () => {
   
+const [selected, setSelected] = useState(options[0])
+
   return (
     <div>
-   {showAccordion()}
-   {showList()}
-   {showDropdown()}
-   {showTranslate()}
-
+   <Route path="/">
+    <Accordion items={items} />
+   </Route>
+   <Route path="/dropdown">
+    <Dropdown label="Select a color" options={options} selected={selected} onSelectedChange={setSelected} />
+   </Route>
+   <Route path="/list">
+    <Search />
+   </Route>
+   <Route path="/translate">
+    <Translate />
+   </Route>
     </div>
   );
 };
