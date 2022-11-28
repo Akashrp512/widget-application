7b66ea29a6f206e90293845e862d5dd3d31ac365
Akash RP <akashrp512@gmail.com>
2022-11-28 10:23:18 +0530

    Added navigation Header

--- src/App.js
+++ src/App.js
@@ -6,34 +6,16 @@ import { items } from './constants';
 import { options } from './constants';
 import Translate from './components/Translate';
 import Route from './components/Route';
+import Header from './components/Header';
 
-const showAccordion =() =>{
-  if(window.location.pathname === '/'){
-    return <Accordion items={items} />
-  }
-}
-const showList =() =>{
-  if(window.location.pathname === '/list'){
-    return <Search />
-  }
-}
-const showDropdown =() =>{
-  if(window.location.pathname === '/dropdown'){
-    return <Dropdown   options={options}  />
-  }
-}
-const showTranslate =() =>{
-  if(window.location.pathname === '/translate'){
-    return <Translate />
-  }
-}
 
 const App = () => {
   
 const [selected, setSelected] = useState(options[0])
 
   return (
-    <div>
+    <div> 
+    <Header />
    <Route path="/">
     <Accordion items={items} />
    </Route>
