5ae775fd4a68d17fb830d7849b4e320479130c0f
Akash RP <akashrp512@gmail.com>
2022-11-28 10:04:19 +0530

    old method of Routing

--- src/App.js
+++ src/App.js
@@ -6,13 +6,35 @@ import { items } from './constants';
 import { options } from './constants';
 import Translate from './components/Translate';
 
+const showAccordion =() =>{
+  if(window.location.pathname === '/'){
+    return <Accordion items={items} />
+  }
+}
+const showList =() =>{
+  if(window.location.pathname === '/list'){
+    return <Search />
+  }
+}
+const showDropdown =() =>{
+  if(window.location.pathname === '/dropdown'){
+    return <Dropdown   options={options}  />
+  }
+}
+const showTranslate =() =>{
+  if(window.location.pathname === '/translate'){
+    return <Translate />
+  }
+}
 
 const App = () => {
   
   return (
     <div>
-   
-   <Translate/>
+   {showAccordion()}
+   {showList()}
+   {showDropdown()}
+   {showTranslate()}
 
     </div>
   );
