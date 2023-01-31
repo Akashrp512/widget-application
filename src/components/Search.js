cd34ec053e2fa4b653ee64117006f116f93ff229
Akash RP <akashrp512@gmail.com>
2023-01-31 20:51:14 +0530

    commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -14,7 +14,7 @@ const Search = () => {
                 list: 'search',
                 origin: '*',
                 format: 'json',
-                srsearch: term,
+                search: term,
             },
             },[]);
 
